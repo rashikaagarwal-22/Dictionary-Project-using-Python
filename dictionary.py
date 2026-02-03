2fb4fd2b261539c25e8aae28f2f6eb00f1a401f0
Rashika Agarwal <agarwalrashika323@gmail.com>
2026-02-03 12:09:39 +0530

    Added get closest match feature

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -10,6 +11,15 @@ def translate(word):
         return data[word.title()]
     elif word.upper() in data:
         return data[word.upper()]
+    elif len(get_close_matches(word,data.keys()))>0:
+        print("Did you mean %s instead" %get_close_matches(word,data.keys())[0])
+        decide = input("Press y for yes or n for no")
+        if(decide=='y'):
+            return data[get_close_matches(word,data.keys)[0]]
+        elif (decide == 'n'):
+            return ("Pugger your paws stepped on wrong keys")
+        else:
+            return ("You have entered wrong input please just enter y or n")
     else:
         print("Pugger your paws stepped on wrong keys")
 
@@ -21,3 +31,4 @@ if type(output)==list:
         print(item)
 else:
     print(output)
+
